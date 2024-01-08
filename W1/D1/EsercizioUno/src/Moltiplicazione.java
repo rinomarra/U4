6a4379c6057b51309f54cbc5e1a69ddddda3c84f
rinomarra <62858445+rinomarra@users.noreply.github.com>
2024-01-08 18:34:14 +0100

    Update EsercizioUno

--- W1/D1/EsercizioUno/src/Moltiplicazione.java
+++ W1/D1/EsercizioUno/src/Moltiplicazione.java
@@ -1,22 +1,43 @@
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Moltiplicazione {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Esercizio 1");
-        //primo metodo
-        int prodotto = moltiplica(5, 5);
-        System.out.println(prodotto);
-        //secondo metodo
-        //terzo metodo
+        int a = 20;
+        int b = 50;
+        int result = multiply(a, b);
+        System.out.println(result);
+
+        System.out.println(concat("bla", 6));
+
+        String[] arr = insertIntoArray(new String[]{"A", "B", "C", "D", "E"}, "aaaaa");
+
+        System.out.println(Arrays.toString(arr));
     }
 
     //Metodo Moltiplica
-    public static int moltiplica(int a, int b) {
+    public static int multiply(int a, int b) {
         return a * b;
     }
-    //Concatena
 
+    public static String concat(String str, int num) {
+        return str + num;
+    }
+
+    public static String[] insertIntoArray(String[] stringArray, String str) {
+        String[] array = new String[6];
+        for (int i = 0; i < stringArray.length +1 ; i++) {
+            if(i < 2) {
+                array[i] = stringArray[i];
+            } else if(i == 2) {
+                array[i] = str;
+            } else {
+                array[i] = stringArray[i - 1];
+            }
+        }
+
+        return array;
+    }
 
 }
