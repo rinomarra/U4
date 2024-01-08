ef0d3dfe79662d1027edd38204e4ed81ce7b9e7b
rinomarra <62858445+rinomarra@users.noreply.github.com>
2024-01-08 18:30:26 +0100

    Add EsercizioDue

---
+++ W1/D1/EsercizioDue/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] inputStrings = new String[3];
+        System.out.println("Insert first string then Enter");
+        inputStrings[0] = scan.nextLine();
+        System.out.println("Insert second string then Enter");
+        inputStrings[1] = scan.nextLine();
+        System.out.println("Insert third string then Enter");
+        inputStrings[2] = scan.nextLine();
+
+        System.out.println("Here the strings concatenated");
+        System.out.println(inputStrings[0] + inputStrings[1] + inputStrings[2]);
+
+        System.out.println("Here they are in reverse order");
+        System.out.println(inputStrings[2] + inputStrings[1] + inputStrings[0]);
+        scan.close();
+    }
+}
