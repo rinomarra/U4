29ffe5a85961fc19e49297a4ea6b4ada49d7cb83
rinomarra <62858445+rinomarra@users.noreply.github.com>
2024-01-09 15:27:29 +0100

    added methods and examples

--- W1/D2/EsercitazioneD2/src/Main.java
+++ W1/D2/EsercitazioneD2/src/Main.java
@@ -79,4 +79,91 @@ public class Main {
     public static int square(int n) {
         return n * n;  // Restituisce il quadrato di n
     }
+
+    // ******************************************** SWITCH-CASE **************************************************
+/*        String favouriteColor = "green";
+        String favouriteDish = "pizza";
+
+        switch (favouriteColor) {
+            case "red": {
+                System.out.println("Il colore preferito è il rosso!");
+                break;
+            }
+            case "yellow": {
+                System.out.println("Il colore preferito è il giallo!");
+                break;
+            }
+            case "blue": {
+                System.out.println("Il colore preferito è il blu!");
+                break;
+            }
+            default: {
+                System.out.println("Il colore non è nè rosso nè giallo nè blu");
+            }
+
+        }*/
+
+/*        if(favouriteDish.equals("pizza")){ <-- Quando si comparano due stringhe meglio usare ".equals" invece che "=="
+
+        }*/
+
+/*        int x = 10, y = 20;
+
+        int z = (x > y) ? -100 : 100;
+
+        switch (z) {
+            case 10: {
+                System.out.println("z é 10");
+                x++;
+                break;
+            }
+            case -100:{
+                System.out.println("z é -100");
+                break;
+            }
+            case 100: {
+                System.out.println("z é 100");
+                x--;
+                break;
+            }
+            case 1000: {
+                System.out.println("z é 1000");
+                x--;
+                break;
+            }
+            default: {
+                System.out.println("Default");
+                x--;
+            }
+        }
+
+        System.out.println("X è " + x);*/
+
+    // ***************************************** LOOPS **********************************
+/*
+        int x = 6, y = 3;
+
+*//*        while (y <= x + 6) {
+            System.out.println(y); // y = 12
+            y += 3; // y = 15 quindi la condizione del while diventa falsa ed esco dal ciclo
+        }*//*
+     */
+/*        int x = 6;
+        do {
+            System.out.println(x = x + 1);
+
+        } while (x < 10);*/
+
+    // System.out.println(x);
+
+/*        for (int i = 1; i <= 10; i++) {
+            if (i % 2 == 0) continue; // col continue saltiamo l'iterazione corrente e passiamo alla prossima
+            System.out.println(i);
+        }*/
+
+/*        for (int i = 1; i <= 10; i++) {
+            if (i == 6) break; // col break usciamo proprio dal loop
+            System.out.println(i);
+        }*/
+
 }
