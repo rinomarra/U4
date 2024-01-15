044fb19a4988103b51649f04dea8a81819d434a8
rinomarra <62858445+rinomarra@users.noreply.github.com>
2024-01-15 14:07:05 +0100

    add Random class

--- W2/D1/TestLib/src/Main.java
+++ W2/D1/TestLib/src/Main.java
@@ -4,12 +4,18 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Random;
+
 public class Main {
 
     //private static final Logger logger = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
 
         final Logger logger = LoggerFactory.getLogger(Main.class);
+        Random  rdm = new Random();
+        //i numeri random di test saranno compresi fra 0 e 9
+        int test = rdm.nextInt(10);
+        logger.info("Numero random: "+test);
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
@@ -18,7 +24,9 @@ public class Main {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
-            logger.debug("Valore attuale di i: "+i);
+            //i numeri di numberRdn saranno compresi fra 0 e il limite imposto dal contatore
+            int numberRdn = rdm.nextInt(0, i);
+            logger.debug("Valore attuale di i: "+numberRdn);
         }
         logger.info("Test informativo: "+i);
         logger.warn("Attenzione alla variabile i: "+i);
